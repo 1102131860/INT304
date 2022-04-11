ff8a131d9a3e9bb1b7261428ab5202ffb20e607e
1102131860 <1102131860@qq.com>
2022-04-11 21:13:07 +0800

    First commitment

---
+++ ClassAndObject.cpp
@@ -0,0 +1,14 @@
+﻿#include<string>
+#include <iostream>
+#include"Student.h";
+using namespace std;
+
+int main()
+{
+    Student s1("John", "12-31-2000", 22);
+    s1.show();
+    Student s2("Jenny", "2-29-1996", 26);
+    s2.show();
+    
+}
+
