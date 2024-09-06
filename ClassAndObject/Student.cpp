2350ad10a25e313824a55a0ec4b5342e434f081c
1102131860 <103301471+1102131860@users.noreply.github.com>
2024-09-06 18:10:52 -0400

    Delete ClassAndObject directory

--- ClassAndObject/Student.cpp
+++
@@ -1,8 +0,0 @@
-#include "Student.h"
-void Student::show() {
-	cout << "Student's name:\t" << name  << "\tage:\t" << age << endl;
-}
-Student::Student(Student& c) {
-	name = c.name;
-	age = c.age;
-}
